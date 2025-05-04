4c5f4e57a2facb62eb5477e0892c99d80013ce62
Rahul Shrimali <rahulshrimali4666@gmail.com>
2025-05-04 08:27:54 +0530

    Added STATIC_ROOT setting

--- django_app/settings.py
+++ django_app/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Custom user model
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
